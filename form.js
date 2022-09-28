378c1a542e09ab8c688f9aba8b74488e68897759
Lashak11 <lkaraulashvili@yahoo.com>
2022-09-28 20:37:11 +0400

    form validation
    add form validation

--- form.js
+++ form.js
@@ -11,20 +11,20 @@ form.addEventListener("submit", function (event) {
 
   if (username.lenght < 5 || username == "") {
     errors.username = " username cant be empty ";
-  }
+  };
 
   let password = document.getElementById("Password").value;
   let password2 = document.getElementById("Password2").value;
 
   if (password != password2) {
     errors.password2 = " passwords dont macth";
-  }
+  };
 
   let checkbox = document.getElementById("checkbox").checked;
 
   if (checkbox == false) {
     errors.checkbox = " you must agree our terms ";
-  }
+  };
 
   form.querySelectorAll(".error-text").forEach((item) => {
     item.innerHTML = "";
@@ -32,14 +32,11 @@ form.addEventListener("submit", function (event) {
 
   for (let item in errors) {
     console.log(item);
-
     let errorDiv = document.getElementById("error_" + item);
-
     if (errorDiv) {
       errorDiv.textContent = errors[item];
     }
-  }
-
+  };
   if (Object.keys(errors).length == 0) {
     form.submit();
   }
